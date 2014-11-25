dcd27bb442f574763c4e9c93528acb008f0b2a9f
kmichala <kmichala@live.unc.edu>
2014-11-25 10:15:06 -0500

    Update server.py

--- server.py
+++ server.py
@@ -63,8 +63,8 @@ for arg in ['name', 'location', 'description']:
 # do we need this at all...?
 #
 update_business_parser = reqparse.RequestParser()
-update_business_parser.add_argument(
-    'category', type=int, default=CATEGORIES.index('shop'))
+#update_business_parser.add_argument(
+#    'category', type=int, default=CATEGORIES.index('shop'))
 
 #
 # specify the parameters for filtering and sorting help requests
@@ -87,7 +87,7 @@ class Business(Resource):
     def patch(self, business_id):
         error_if_business_not_found(business_id)
         business=businesses[business_id]
-        update = update_business_parser.parse_args()
+#        update = update_business_parser.parse_args()
         business['name'] = update['name']
         business['location'] = update['location']
         business['URL'] = update['URL']
